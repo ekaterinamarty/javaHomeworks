d48ca2429e4f22e2144b83e62b1d98510ae8364c
Ekaterina <sonyamasonya@gmail.com>
2022-04-05 17:30:18 +0300

    HW3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int[] array = new int [Integer.MAX_VALUE - 8];
+	// write your code here;
     }
 }
